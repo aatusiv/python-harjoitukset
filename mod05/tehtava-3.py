e9baace14fbc73c0a388cc3fbaff7bf21aa2b5f9
Aatu Sivonen <aatusivonen@outlook.com>
2026-09-06 18:13:55 +0300

    Added tehtava-3.py

---
+++ mod05/tehtava-3.py
@@ -0,0 +1,15 @@
+values = []
+
+while True:
+    value = input("Anna luku: ")
+    if value == "":
+        break
+    else:
+        values.append(value)
+
+values.sort()
+try:
+    print(f"Pienin luku on {values[0]} ja suurin luku on {values[-1]}.")
+except:
+    print("Et ole antanut lukuja.")
+    
