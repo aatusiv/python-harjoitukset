7c54aeed8f3fbc64c0f25baaf959594d19039934
Aatu Sivonen <aatusivonen@outlook.com>
2026-09-07 18:50:17 +0300

    Added commands to menu

--- peliprojekti/pelaaja-tiedot.py
+++ peliprojekti/pelaaja-tiedot.py
@@ -1,3 +1,5 @@
+import random
+
 name = input("Anna nimesi: ")
 age = int(input("Anna ikäsi: "))
 
@@ -6,8 +8,21 @@ if age < 12:
 else:
     print(f"Hei, {name}")
     while True:
-        print("1. Aloita\n2. Lopeta")
-        selection = input("Kirjoita komento: ")
-        if selection == "Lopeta":
+        print("- Kolikko\n- Kahvi\n- Villapaita\n- Lopeta")
+        selection = input("Kirjoita komento: ").lower()
+        if selection == "lopeta":
+            print("Suljetaan peli.")
             break
-# print(f"Nimesi on {name} ja ikäsi {age}.")
+        elif selection == "kolikko":
+            result = random.choice(["kruuna", "klaava"])
+            print(f"\nKolikon heiton tulos: {result}!\n")
+        elif selection == "kahvi":
+            jokes = ["Miksi kutsutaan isoa töppäystä Pauligilla?\n– Juhlamokaksi.",
+                     "Tarjoilija koputti hotellihuoneen oveen ja kysyi:\n– Haluatteko kahvia vuoteeseen?Asukas vastasi: \n– Tuota, mieluummin kuppiin, kiitos.",
+                     "Miten kahvi ja opiskelija eroavat toisistaan?\n– Kahvi suodattuu, opiskelija ei välttämättä."]
+            result = random.choice(jokes)
+            print(f"\n{result}\n")
+        elif selection == "villapaita":
+            print("\nSakarin\n")
+        else:
+            print("Tuntematon komento. Yritä uudelleen.")
