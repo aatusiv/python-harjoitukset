a97c5777ca2938bf7cb1345a0782202d6a0ab8c6
Aatu Sivonen <aatusivonen@outlook.com>
2026-09-17 09:44:22 +0300

    Added tehtava-2.py

---
+++ mod08/tehtava-2.py
@@ -0,0 +1,13 @@
+all_names = set()
+
+while True:
+    name = input("Anna nimi: ")
+    if name == "":
+        print("Ohjelma lopetetaan.")
+        break
+
+    if name in all_names:
+        print("Aiemmin syötetty nimi.")
+    else:
+        all_names.add(name)
+        print("Uusi nimi.")
