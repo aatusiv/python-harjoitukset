709a8a6e03161e37e287c142587d1d1ef2f152c3
Aatu Sivonen <aatusivonen@outlook.com>
2026-09-17 11:11:48 +0300

    Added tehtava-4.py

---
+++ mod09/tehtava-4.py
@@ -0,0 +1,56 @@
+import random
+
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.matka = 0
+
+    def kiihdytä(self, nopeus):
+        if nopeus > 0:
+            if self.nopeus + nopeus > self.huippunopeus:
+                self.nopeus = self.huippunopeus
+                # print(f"Nopeus on nyt huipussaan: {self.nopeus}km/h.")
+            else:
+                self.nopeus += nopeus
+                # print(f"Nopeus on nyt: {self.nopeus}km/h.")
+
+        elif nopeus < 0:
+            # matematiikan säännöt ex. 150 + (-200)
+            if self.nopeus + nopeus <= 0:
+                self.nopeus = 0
+                # print(f"Nopeus on nyt nolla: {self.nopeus}km/h.")
+            else:
+                self.nopeus += nopeus
+                # print(f"Nopeus on nyt: {self.nopeus}km/h.")
+
+    def kulje(self, tunnit):
+        self.matka += self.nopeus * tunnit
+        # print(f"Kuljettu matka: {self.matka}km.")
+
+cars = []
+
+# Luo auto-oliot
+for x in range(10):
+    cars.append(Auto(f"ABC-{x}", random.randint(100,200)))
+
+# Luodaan "lippu"
+winner = False
+while winner is False:
+
+    # Muuttaa nopeutta satunnaisesti -10 ja 15kmh väliltä
+    for x in range(len(cars)):
+        cars[x].kiihdytä(random.randint(-10,15))
+
+    # Kuljettaa tunnin ajan
+    for x in range(len(cars)):
+        cars[x].kulje(1)
+
+    # Tarkastaa kuljetun matkan
+    for x in range(len(cars)):
+        if cars[x].matka >= 10000:
+            print(f"{cars[x].rekisteritunnus} on voittanut kilpailun")
+            for x in range(len(cars)):
+                print(f"Rekisteritunnus: {cars[x].rekisteritunnus}, huippunopeus: {cars[x].huippunopeus}km/h, nopeus: {cars[x].nopeus}km/h, matka: {cars[x].matka}km")
+            winner = True
