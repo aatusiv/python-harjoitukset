1bc3967b8734447cb5dcf526e8dc73117552a7a1
Aatu Sivonen <aatusivonen@outlook.com>
2026-09-18 10:01:58 +0300

    Added tehtava-3.py

---
+++ mod10/tehtava-3.py
@@ -0,0 +1,52 @@
+class Hissi:
+    def __init__(self):
+        self.kerros = 0
+
+    def kerros_ylös(self):
+        self.kerros += 1
+        print(f"Kerros on: {self.kerros}.")
+
+    def kerros_alas(self):
+        self.kerros -= 1
+        print(f"Kerros on: {self.kerros}.")
+
+    def siirry_kerrokseen(self, kerros_nro):
+        if self.kerros < kerros_nro:    
+            for x in range(kerros_nro-self.kerros):
+                self.kerros_ylös()
+        else:
+            for x in range(self.kerros - kerros_nro):
+                self.kerros_alas()
+
+
+class Talo:
+    def __init__(self, alin_kerros, ylin_kerros, hissi_lkm):
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
+        self.hissi_lkm = hissi_lkm
+
+        self.hissit = []
+        for x in range(self.hissi_lkm):
+            self.hissit.append(Hissi())
+
+    def aja_hissiä(self, hissi_nro, kohdekerros):
+        self.hissit[hissi_nro].siirry_kerrokseen(kohdekerros)
+
+    def palohälytys(self):
+        print("Palohälytys, kaikki hissit palaa alimpaan kerrokseen.")
+        for x in range(self.hissi_lkm):
+            self.hissit[x].siirry_kerrokseen(1)
+
+talo = Talo(1,9,5)
+
+while True:
+    hissi_selection = int(input("Valitse hissinumero: "))
+    if hissi_selection <= 0:
+        print("Ohjelma lopetetaan.")
+        break
+    elif hissi_selection == 9999: # Hissit palaa alimpaan kerrokseen syöttämällä hissinro 9999
+        talo.palohälytys()
+        break
+    floor_selection = int(input("Valitse kerros: "))
+
+    talo.aja_hissiä(hissi_selection-1, floor_selection)
