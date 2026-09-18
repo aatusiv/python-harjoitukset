ef93e10552adc011d7d5f00b94fcbcd6468e37d0
Aatu Sivonen <aatusivonen@outlook.com>
2026-09-18 13:12:09 +0300

    Added tehtava-1.py

---
+++ mod11/tehtava-1.py
@@ -0,0 +1,32 @@
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, päätoimittaja):
+        self.päätoimittaja = päätoimittaja
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        print(f"Nimi: {self.nimi}\nPäätoimittaja: {self.päätoimittaja}")
+
+class Kirja(Julkaisu):
+    def __init__(self, nimi, sivumäärä, päätoimittaja):
+        self.sivumäärä = sivumäärä
+        self.päätoimittaja = päätoimittaja
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        print(f"Nimi: {self.nimi}\nPäätoimittaja: {self.päätoimittaja}\nSivumäärä: {self.sivumäärä} sivua")
+
+def main():
+    lehti = Lehti("Aku Ankka", "Aki Hyyppä")
+    kirja = Kirja("Hytti n:o 6", "Rosa Liksom", 200)
+
+    lehti.tulosta_tiedot()
+    print("\n")
+    kirja.tulosta_tiedot()
+
+
+if __name__ == "__main__":
+    main()
